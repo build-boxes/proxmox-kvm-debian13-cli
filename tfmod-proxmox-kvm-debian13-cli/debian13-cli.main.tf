c37b45028334cbe2e78b104c70d8164bb214066f
Hammad Rauf <rauf.hammad@gmail.com>
2026-01-29 15:57:40 -0500

    Module, WIP 3

--- tfmod-proxmox-kvm-debian13-cli/debian13-cli.main.tf
+++ tfmod-proxmox-kvm-debian13-cli/debian13-cli.main.tf
@@ -258,7 +258,8 @@ resource "null_resource" "run_ansible_playbook" {
   depends_on = [null_resource.wait_4_apt]
   provisioner "local-exec" {
     #interpreter = ["/bin/bash"]
-    working_dir = "./ansible-playbooks"
+    # Use the module path so the playbooks are found whether the module is local or fetched into .terraform/modules
+    working_dir = "${path.module}/ansible-playbooks"
     command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u '${var.superuser_username}' -i '${local.host_ip},' --private-key ${var.pvt_key_file} -e 'pub_key=${var.pub_key_file}' ansible_main.yml -e 'install_docker=${var.docker_intalled}'"
   }
 }
